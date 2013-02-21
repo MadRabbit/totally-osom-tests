d94fec27e5db5b79fa0dac7c3acc3a6886ce604a
Nikolay Nemshilov <nemshilov@gmail.com>
2013-02-21 11:53:49 +1100

    Adding a bunch of standard assertions

--- lib/tots/assert.rb
+++ lib/tots/assert.rb
@@ -9,22 +9,47 @@ class TOTS::Spec
   end
 
   def assert_equal(v1, v2, msg=nil)
-    unless v1 == v2
-      raise TOTS::Fail, "#{v1.inspect} supposed to be equal to #{v2.inspect}"
-    end
+    assert v1 == v2, msg || "#{v1.inspect} supposed to be equal to #{v2.inspect}"
   end
 
-  def assert_raises(type, &block)
+  def assert_empty(v, msg=nil)
+    assert_respond_to v, :empty?
+    assert v.empty?, msg || "#{v.inspect} supposed to be empty"
+  end
+
+  def assert_respond_to(obj, method, msg=nil)
+    assert obj.respond_to?(method), msg || "#{obj.inspect} supposed to respond to the #{method} method"
+  end
+
+  def assert_nil(v, msg=nil)
+    assert v === nil, msg || "#{v.inspect} supposed to be nil"
+  end
+
+  def assert_includes(list, value, msg=nil)
+    assert_respond_to list, :include?
+    assert list.include?(value), msg || "#{list.inspect} supposed to include #{value.inspect}"
+  end
+
+  def assert_match(obj, re, msg=nil)
+    assert_respond_to re, :=~
+    assert re =~ obj, msg || "#{obj.inspect} supposed to match #{re.inspect}"
+  end
+
+  def assert_instance_of(obj, klass, msg=nil)
+    assert obj.instance_of?(klass), msg || "#{obj.inspect} supposed to be instance of #{klass}"
+  end
+
+  def assert_raises(type, msg=nil, &block)
     raise ArgumentError, "no block given" if !block_given?
 
     begin
       yield
 
-      raise TOTS::Fail, "expected to raise #{type}, but nothing was raised"
+      assert false, msg || "expected to raise #{type}, but nothing was raised"
 
     rescue Exception => e
       if type && !(e.is_a?(type))
-        raise TOTS::Fail, "expected to raise #{type}, but got #{e.class} instead"
+        assert false, msg || "expected to raise #{type}, but got #{e.class} instead"
       end
     end
   end
--- test/tots/assert_test.rb
+++ test/tots/assert_test.rb
@@ -23,6 +23,74 @@ describe TOTS::Spec do
       end
     end
 
+    describe "assert_empty" do
+      it "must pass correctly" do
+        assert_empty ''
+      end
+
+      it "must fail with a non-empty objects" do
+        assert_raises(TOTS::Fail) { assert_empty(' ') }
+      end
+
+      it "must fail with objects without the :empty? method" do
+        assert_raises(TOTS::Fail) { assert_empty(nil) }
+      end
+    end
+
+    describe "assert_respond_to" do
+      it "must pass correctly" do
+        assert_respond_to '', :size
+      end
+
+      it "must fail when method is missing" do
+        assert_raises(TOTS::Fail) { assert_respond_to('', :non_existing_method) }
+      end
+    end
+
+    describe "assert_nil" do
+      it "must pass correctly" do
+        assert_nil nil
+      end
+
+      it "must fail with non nils" do
+        assert_raises(TOTS::Fail) { assert_nil(false) }
+        assert_raises(TOTS::Fail) { assert_nil('') }
+        assert_raises(TOTS::Fail) { assert_nil(0) }
+      end
+    end
+
+    describe "assert_includes" do
+      it "must pass correctly" do
+        assert_includes [1,2,3], 2
+      end
+
+      it "must fail correctly" do
+        assert_raises(TOTS::Fail) { assert_includes([1,2,3], 4) }
+        assert_raises(TOTS::Fail) { assert_includes(nil, 4) }
+      end
+    end
+
+    describe "assert_match" do
+      it "must pass correctly" do
+        assert_match "22", /2/
+      end
+
+      it "must fail correctly" do
+        assert_raises(TOTS::Fail) { assert_match("22", /3/) }
+        assert_raises(TOTS::Fail) { assert_match("22", nil) }
+      end
+    end
+
+    describe "assert_instance_of" do
+      it "must pass correctly" do
+        assert_instance_of '', String
+      end
+
+      it "must fail correctly" do
+        assert_raises(TOTS::Fail) { assert_instance_of('', Integer) }
+      end
+    end
+
     describe "assert_raises" do
       it "must pass when something raised" do
         assert_raises(Exception) { raise Exception }
